eb061eb7413dd6ad285b38e1d224c9d20bb33ea1
Jason Long <jlong5795@gmail.com>
2020-03-24 15:06:19 -0500

    More stuff to fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import "./sass/index.scss";
 import Navigation from "./components/navigation";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
+//import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import AddAccount from "./components/AddAccount";
+//import AddAccount from "./components/AddAccount";
 
 
-const routes = (
+/*const routes = (
   <Router>
     <Switch>
       <Route path="/add-acct" component={AddAccount} />
     </Switch>
   </Router>
-)
+)*/
 
 export default function App() {
 
@@ -22,7 +22,7 @@ export default function App() {
       <Navigation />
    
     <main>
-      <Dashboard></Dashboard>
+      <Dashboard />
     </main>
      </>
   );
