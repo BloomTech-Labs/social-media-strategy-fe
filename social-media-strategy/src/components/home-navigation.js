0ca2b2928eb4fe2c9b38986cd197545faa63ac1f
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-03-24 15:19:21 -0400

    navigation styling

--- social-media-strategy/src/components/home-navigation.js
+++ social-media-strategy/src/components/home-navigation.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Switch, Route } from "react-router-dom";
 
 const HomeNav = () => {
+  //   const [home, setHome] = useState(true);
+  //   const [social, setSocial] = useState(false);
+  //   const [analytics, setAnalytics] = useState(false);
+
+  //   const homeHandler = () => {
+  //     setHome(true);
+  //     setSocial(false);
+  //     setAnalytics(false);
+  //   };
+  //   const socialHandler = () => {
+  //     setHome(false);
+  //     setSocial(true);
+  //     setAnalytics(false);
+  //   };
+  //   const analyticsHandler = () => {
+  //     setHome(false);
+  //     setSocial(false);
+  //     setAnalytics(true);
+  //   };
+
   return (
-    <div className="homeNavContainer">
-      <ul>
-        <NavLink to="/home/dashboard">
+    <div id="homeNavContainer">
+      <ul className="homeNavUl">
+        <NavLink
+          className="homeLinkNav"
+          to="/home/dashboard"
+          //   onClick={homeHandler}
+        >
           <li>Categories</li>
         </NavLink>
-        <NavLink to="/home/que">
+        <NavLink
+          className={"homeLinkNav"}
+          to="/home/que"
+          //   onClick={socialHandler}
+        >
           <li>Social Board</li>
         </NavLink>
-        <NavLink to="/home/analytics">
+        <NavLink
+          className="homeLinkNav"
+          to="/home/analytics"
+          //   onClick={analyticsHandler}
+        >
           <li>Analytics</li>
         </NavLink>
       </ul>
