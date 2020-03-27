cfd34efa365137b4834f0f9672a234c53f814666
Jose <Jrivera6869@yahoo.com>
2020-03-26 20:41:25 -0400

    changing

--- social-media-strategy/src/App.js
+++ social-media-strategy/src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./sass/index.scss";
-import Navigation from "./components/navigation";
+import React from 'react';
+import './sass/index.scss';
+import Navigation from './components/navigation';
+import Register_Login from './components/Register_Login';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Register_Login />
     </>
   );
 }
