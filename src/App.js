89efd88f2491b7be5f6d335cea2f3aa5b89af9be
Jose <Jrivera6869@yahoo.com>
2020-03-27 00:44:49 -0400

    cant see login

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ export default function App() {
   return (
     <div className='container'>
       <Switch>
-        <Route path='/login'>
-          <REGISTER_LOGIN />
-        </Route>
         <Route exact path='/'>
           <div className='nav'>
             <Navigation />
@@ -31,6 +28,9 @@ export default function App() {
           <div className='dash'>
             <Dashboard />
           </div>
+          <Route path='/login'>
+            <REGISTER_LOGIN />
+          </Route>
         </Route>
       </Switch>
     </div>
