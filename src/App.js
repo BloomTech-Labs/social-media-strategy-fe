f9af4c8f7d4f06d4ecbca7b245b59d959b282c8e
jrive204 <jrive204@gmail.com>
2020-04-08 18:41:14 -0400

    changes idk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { sort } from "./actions";
 import styled from "styled-components";
@@ -50,56 +50,58 @@ const App = (props) => {
   };
 
   return (
-    <div className='columns is-gapless'>
-      <div className='column is-2'>
+    <div className="columns is-gapless">
+      <div className="column is-2">
         <Navigation />
       </div>
-      <Route exact path='/callback'>
+      <Route exact path="/callback">
         <Callback />
       </Route>
       <Switch>
-        <Route path='/login'>
+        <Route path="/login">
           <REGISTER_LOGIN />
         </Route>
       </Switch>
-      <div className='column is-3'>
+      <div className="column is-3">
         <Dashboard />
       </div>
-      <div className='column drag-drop-content'>
-        <div className='column select-a-header'>
-          <h1 className='headers'>Buckets</h1>
-          <h3 className='unselected-headers'>Social Board Queue</h3>
-          <h3 className='unselected-headers'>Analytics</h3>
+      <div className="column drag-drop-content">
+        <div className="column select-a-header">
+          <h1 className="headers">Buckets</h1>
+          <h3 className="unselected-headers">Social Board Queue</h3>
+          <h3 className="unselected-headers">Analytics</h3>
         </div>
-      <ActionButton className="column is-2 headers" topic />
-      <DragDropContext onDragEnd={onDragEnd}>
-
-        <Droppable className="columns" droppableId='all-topics' direction='horizontal' type='topic'>
-          {provided => (
-            <TopicsContainer
-              // style={{"overflow":"scroll"}}
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-
-              <div className='column topics'>
-                {props.topics.map((topic, index) => (
-                  <TopicBucket
-                    className={`${topic.id}`}
-                    key={topic.id}
-                    topicId={topic.id}
-                    topic={topic}
-                    cards={topic.cards}
-                    index={index}
-                  />
-                ))}
-
-              </div>
-              {provided.placeholder}
-            </TopicsContainer>
-          )}
-        </Droppable>
-      </DragDropContext>
+        <ActionButton className="column is-2 headers" topic />
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable
+            className="columns"
+            droppableId="all-topics"
+            direction="horizontal"
+            type="topic"
+          >
+            {(provided) => (
+              <TopicsContainer
+                // style={{"overflow":"scroll"}}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                <div className="column topics">
+                  {props.topics.map((topic, index) => (
+                    <TopicBucket
+                      className={`${topic.id}`}
+                      key={topic.id}
+                      topicId={topic.id}
+                      topic={topic}
+                      cards={topic.cards}
+                      index={index}
+                    />
+                  ))}
+                </div>
+                {provided.placeholder}
+              </TopicsContainer>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -6,18 +5,23 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 // imports for redux setup
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { reducer } from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { reducer } from "./reducers";
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
