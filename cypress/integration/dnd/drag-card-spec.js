6ddfddc97d140b66aff7efcb369c0496f383b3cf
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-04-10 14:25:01 -0400

    refactored cypress test, deleted examples

--- cypress/integration/dnd/drag-card-spec.js
+++ cypress/integration/dnd/drag-card-spec.js
@@ -1,24 +1,6 @@
-// describe("testing drag feature", function() {
-//   it("takes a card and places in a new topic", function() {
-//     cy.visit("http://localhost:3000/home");
-//     cy.get(".sc-AxjAm.gYYZVf")
-//       .first()
-//       .trigger("dragstart");
-//     // cy.get(".sc-AxgMl.breYhc")
-//     //   .last()
-//     //   .trigger("drop");
-//     cy.get(".sc-AxjAm.gYYZVf")
-//       .last()
-//       .trigger("dragover")
-//       .trigger("dragenter")
-//       .trigger("drop");
-//   });
-// });
-
 describe("testing drag feature", function() {
   it("takes a card and places in a new topic", function() {
     cy.visit("http://localhost:3000/home");
-
     function movePiece(x, y) {
       cy.get(".sc-AxjAm.pHAFq")
         .first()
@@ -32,28 +14,6 @@ describe("testing drag feature", function() {
         )
         .trigger("mouseup", { force: true });
     }
-    // cy.get(".sc-AxjAm.gYYZVf")
-    //   .first()
-    //   .trigger("mousedown", { which: 1 })
-    //   .trigger("dragstart", { dataTransfer })
-    //   .trigger("drag", {});
-
-    // cy.get(".sc-AxjAm.gYYZVf")
-    //   .last()
-    //   .trigger("dragover", { dataTransfer })
-    //   .trigger("drop", { dataTransfer })
-    //   .trigger("dragend", { dataTransfer })
-    //   .trigger("mouseup", { which: 1 });
-
     movePiece(1037, 750);
-
-    // cy.get(".sc-AxgMl.breYhc")
-    //   .last()
-    //   .trigger("drop");
-    //   cy.get(".sc-AxjAm.gYYZVf")
-    //     .last()
-    //     .trigger("dragover")
-    //     .trigger("dragenter")
-    //     .trigger("drop");
   });
 });
