2971b6c9342b019f70ee5c3d24640618f455a353
Jessica Wolff <jleahwolff@gmail.com>
2020-04-23 09:46:57 -0500

    page done. created footer for team page.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -28,10 +28,27 @@ const Landing = () => {
             </div>
         </div>
         <div className='center-message'>
+            <div className="text-align-left">
             <h1 className='main-text'>A fresh take on social media management</h1>
             <h4 className='secondary-text'>Discover how to develop your brand and manage your digital marketing strategy</h4>
-            <Button>Start here</Button>
+            <NavLink 
+            className="start-here"
+            to='/'
+            >
+                Start here
+            </NavLink>
+            </div>
         </div>
+        <footer className='sticky-footer'>
+            <div className='text-align'>
+            <p> All rights reserved SoMe ®</p>
+            <div className='footer-nav'>
+            <NavLink className='team-link' to='/about-us'>Team</NavLink>
+            <NavLink className='team-link' to='/about-us'>Github</NavLink>
+            <NavLink className='team-link' to='/about-us'>Lambda</NavLink>             
+            </div>
+            </div>
+        </footer>
         </div>
     )
 };
