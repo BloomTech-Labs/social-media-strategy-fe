280987a56944d95f76c9c77b00724f3a05093a94
Jason Long <jlong5795@gmail.com>
2020-04-23 12:36:30 -0500

    Completed clean up of Topic Buckets. Required creation of a new component to simplify code.

--- src/components/TopicBucket.js
+++ src/components/TopicBucket.js
@@ -1,196 +1,62 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import TopicCard from "./TopicCard";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import ActionButton from "./ActionButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import CreateIcon from "@material-ui/icons/Create";
+import TopicTitle from "./TopicTitle";
 
-import {
-  fetchTopics,
-  updateTopics,
-  deleteTopics,
-  editTopic
-} from "../actions";
+import { fetchTopics, updateTopics, deleteTopics, editTopic } from "../actions";
 
-const TopicBucket = props => {
-  const [content, setcontent] = useState({ name: props.topic.title });
-  const [editing, setediting] = useState(false);
-
-  const handleChange = e => {
-    e.preventDefault();
-    setcontent({ ...content, [e.target.name]: e.target.value });
-  };
-
-  const submitForm = (e) => {
-    e.preventDefault();
-    props.editTopic(props.topid.id, content.name)
-    setcontent({ name: content.name })
-    setediting(!editing)
-  };
-
-  let scrollCondition = props?.cards?.length > 4;
+const TopicBucket = (props) => {
+  let scrollId = "";
+  if (props?.cards?.length > 4) {
+    scrollId = "topic-scroll";
+  }
 
   return (
-    <>
-      <Draggable draggableId={String(props.topic.id)} index={props.index}>
-        {provided => (
-          <div
-            className={`${props.className} container`}
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-            {...provided.dragHandleProps}
-          >
-            <Droppable droppableId={String(props.topic.id)} type="card">
-              {provided => (
-                <>
-                  {scrollCondition ? (
-                    <>
-                      <div className='title'>
-                        {props.topic.title !== "Drafts" ? (
-                          <span>
-                            <span className={`deleteTopic`}>
-                              <DeleteIcon
-                                style={{ "margin-right": "20" }}
-                                onClick={() =>
-                                  props.deleteTopics(props.topic.id)
-                                }
-                              />
-                            </span>
-                            <CreateIcon onClick={() => setediting(!editing)} />
-                            {!editing ? (
-                              props.topic.title // if not editing show this
-                            ) : (
-                              // else show this
-                              <>
-                                <form onSubmit={submitForm}>
-                                  <input
-                                    type="text"
-                                    name="name"
-                                    value={content.name}
-                                    onChange={handleChange}
-                                    defaultValue={props.topic.title}
-                                  />
-                                  &nbsp;{" "}
-                                  <span
-                                    onClick={() => setediting(!editing)}
-                                    style={{
-                                      color: "red",
-                                      fontWeight: "bolder",
-                                      padding: "5px"
-                                    }}
-                                  >
-                                    x
-                                  </span>
-                                </form>
-                              </>
-                            )}
-                          </span>
-                        ) : (
-                          props.topic.title
-                        )}
-                      </div>
-
-                      <div className='cardlist'
-                        id="topic-scroll"
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {props.cards.map((card, index) => (
-                          <TopicCard
-                            className={`${card.id}`}
-                            key={card.id}
-                            card={card}
-                            index={index}
-                            id={card.id}
-                          />
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <h4 className='title'>
-                        {props.topic.title !== "Drafts" ? (
-                          <span className="editTopics">
-                            <span className="topicIcons">
-                              <span className={`deleteTopic`}>
-                                <DeleteIcon
-                                  style={{ "margin-right": "20" }}
-                                  onClick={() =>
-                                    props.deleteTopics(props.topic.id)
-                                  }
-                                />
-                              </span>
-                              <CreateIcon
-                                className={`editTopic`}
-                                onClick={() => setediting(!editing)}
-                              />
-                            </span>
-                            {!editing ? (
-                              <span className="topicTitle">
-                                {props.topic.title}
-                              </span>
-                            ) : (
-                              <>
-                                <form onSubmit={submitForm}>
-                                  <input
-                                    type="text"
-                                    name="name"
-                                    value={content.name}
-                                    onChange={handleChange}
-                                  />
-                                  &nbsp;{" "}
-                                  <span
-                                    onClick={() => setediting(!editing)}
-                                    style={{
-                                      color: "red",
-                                      fontWeight: "bolder",
-                                      padding: "5px"
-                                    }}
-                                  >
-                                    x
-                                  </span>
-                                </form>
-                              </>
-                            )}
-                          </span>
-                        ) : (
-                          props.topic.title
-                        )}
-                      </h4>
-                      <div className='cardlist'
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {props?.cards?.map((card, index) => (
-                          <TopicCard
-                            className={`${card.id}`}
-                            key={card.id}
-                            card={card}
-                            index={index}
-                            id={card.id}
-                          />
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    </>
-                  )}
-                  <ActionButton topicId={props.topicId} />
-                </>
-              )}
-            </Droppable>
-          </div>
-        )}
-      </Draggable>
-    </>
+    <Draggable draggableId={String(props.topic.id)} index={props.index}>
+      {(provided) => (
+        <div
+          className={`${props.className} container`}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+        >
+          <Droppable droppableId={String(props.topic.id)} type='card'>
+            {(provided) => (
+              <>
+                <TopicTitle topic={props.topic} />
+                <div
+                  className='cardlist'
+                  id={scrollId}
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {props.cards.map((card, index) => (
+                    <TopicCard
+                      className={`${card.id}`}
+                      key={card.id}
+                      card={card}
+                      index={index}
+                      id={card.id}
+                    />
+                  ))}
+                  {provided.placeholder}
+                </div>
+                <ActionButton topicId={props.topicId} />
+              </>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     user: state.user,
-    topics: state.topics
+    topics: state.topics,
   };
 };
 
@@ -198,5 +64,5 @@ export default connect(mapStateToProps, {
   deleteTopics,
   fetchTopics,
   updateTopics,
-  editTopic
+  editTopic,
 })(TopicBucket);
