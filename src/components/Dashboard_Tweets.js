3bf96d5f6700ee81a5f794f0a050f954411e008b
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-04-27 18:16:03 -0400

    updated scheduled, landing, team

--- src/components/Dashboard_Tweets.js
+++ src/components/Dashboard_Tweets.js
@@ -2,59 +2,54 @@ import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import styled from 'styled-components';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import { makeStyles } from '@material-ui/core/styles';
+
 // import Container from './TopicCard.js';
 const TweetContainer = styled.div`
-  background-color: white;
-  border-radius: 0.5rem;
-  padding: 1rem 1rem;
-  margin: 0.5rem auto;
-  width: 80%;
-  text-align: center;
-  border: 2px solid #1ca1f2;
-
-  .scheduled {
-    color: lightgrey;
-    font-size: 0.8rem;
-    padding: 0.6rem 0rem;
-  }
-
   .postText {
     padding: 2rem 0rem;
   }
 `;
 
-const HandleInfo = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-`;
+const useStyles = makeStyles(theme => ({
+  tweetCont: {
+    backgroundColor: 'white',
+    borderRadius: '0.5rem',
+    padding: '1rem 1rem 0',
+    margin: '0.5rem auto',
+    width: '80%',
+    border: '2px solid #1ca1f2'
+  },
 
-const TopBar = styled.div`
-  display: flex;
-  border-bottom: 1px solid lightgrey;
+  scheduled: {
+    fontSize: '.75rem',
+    fontWeight: '400',
+    marginTop: '2.5rem',
+    textAlign: 'right',
+    color: '#e85556'
+  },
 
-  a {
-    padding: 0 1rem;
-    display: flex;
-    align-items: center;
+  postText: {
+    textAlign: 'center',
+    fontSize: '1.4rem'
   }
+}));
 
-  .proPic {
-    border-radius: 50%;
-    margin-bottom: 0.5rem;
-    width: 40px;
-  }
-`;
-
-const Title = styled.span`
+const Title = styled.div`
   color: white;
   font-weight: bold;
-  font-size: 1.5rem;
-  text-align: center;
-  margin: 1rem 0 1rem 1.5rem;
+  font-size: 1.6rem;
+  text-align: left;
+  padding: 5rem 0 3rem 2.5rem;
 `;
 
-const timeformat = (toFormat) => {
+const timeformat = toFormat => {
   let date = new Date(toFormat);
   var h = date.getHours();
   var m = date.getMinutes();
@@ -66,7 +61,7 @@ const timeformat = (toFormat) => {
   return mytime;
 };
 
-const dateFormat = (dateObj) => {
+const dateFormat = dateObj => {
   const date = new Date(dateObj);
 
   let formatted = `${date.getMonth() +
@@ -75,9 +70,10 @@ const dateFormat = (dateObj) => {
   return formatted;
 };
 
-const Dashboard_Tweets = (props) => {
+const Dashboard_Tweets = props => {
   const [tweets, setTweets] = useState([]);
   const userID = props.currentUser.subject;
+  const classes = useStyles();
 
   const twitHandle = props.user.accounts[0].screen_name;
 
@@ -86,7 +82,7 @@ const Dashboard_Tweets = (props) => {
   useEffect(() => {
     axiosWithAuth()
       .get(`/posts/${userID}/user`)
-      .then((res) => {
+      .then(res => {
         const completedTweets = [];
 
         for (let post in res?.data) {
@@ -96,71 +92,70 @@ const Dashboard_Tweets = (props) => {
         }
         setTweets(orderTweets(completedTweets));
       })
-      .catch((err) => console.log(err.message));
+      .catch(err => console.log(err.message));
   }, [updateTrue]);
 
-  const orderTweets = (tweetArray) => {
+  const orderTweets = tweetArray => {
     let original = tweetArray;
 
     original = original
-      .filter((tweet) => {
+      .filter(tweet => {
         // const compare = new Date(tweet.date);
 
         return tweet.date.length > 0;
       })
-      .filter((tweet) => {
+      .filter(tweet => {
         const compare = new Date(tweet.date);
 
         return compare > new Date();
       });
-
     console.log('ORIGINAL PRE SORT', original);
     original.sort(function compare(a, b) {
       var dateA = new Date(a.date);
       var dateB = new Date(b.date);
       return dateB - dateA;
     });
-
     return original;
   };
-
+  console.log('props from dashbaord', props);
   return (
     <div>
-      <Title>My Scheduled Posts</Title>
-      {tweets.map((tweet) => (
-        <TweetContainer key={Date.now() + Math.random()}>
-          <TopBar>
-            <img
-              className='proPic'
-              src={props.user.accounts[0].profile_img}
-              alt=''
-            ></img>
-            <HandleInfo>
+      {tweets.length < 1 ? (
+        <Title>Your Scheduled Tweets Appear Here</Title>
+      ) : (
+        <Title>My Scheduled Posts</Title>
+      )}
+      {tweets.map(tweet => (
+        <Card className={classes.tweetCont} key={Date.now() + Math.random()}>
+          <CardHeader
+            avatar={
+              <Avatar
+                className="proPic"
+                src={props.user.accounts[0].profile_img}
+                alt=""
+              />
+            }
+            action={
+              <TwitterIcon style={{ color: '#3282B8', 'margin-top': '1rem' }} />
+            }
+            title={props.user.accounts[0].name}
+            subheader={
               <a
                 style={{ color: '#3282B8', textDecoration: 'none' }}
                 href={`https://twitter.com/${twitHandle}`}
-                alt=''
+                alt=""
               >
                 <span>@{`${twitHandle}`}</span>
               </a>
-            </HandleInfo>
-
-            <div
-              style={{
-                color: 'dodgerblue',
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <TwitterIcon />
+            }
+          />
+          <CardContent>
+            <div className={classes.postText}>{`${tweet.post_text}`}</div>
+            <div className={classes.scheduled}>
+              Scheduled: {`${dateFormat(tweet.date)}`}
             </div>
-          </TopBar>
-
-          <div className='scheduled'>
-            Scheduled: {`${dateFormat(tweet.date)}`}
-          </div>
-          <div className='postText'>{`${tweet.post_text}`}</div>
-        </TweetContainer>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
