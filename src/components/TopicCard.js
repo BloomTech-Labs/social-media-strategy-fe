8173ce41da07ac6f281d5612c24e02ca066bc964
Jason Long <jlong5795@gmail.com>
2020-04-28 12:29:22 -0500

    Step one of reducing cognitive complexity of TopicCard

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -17,6 +17,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Backdrop from '@material-ui/core/Backdrop';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
+import CardModal from './CardModal';
+
 import { Fade, Menu, Tooltip, IconButton, Typography } from '@material-ui/core';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import Grid from '@material-ui/core/Grid';
@@ -54,12 +56,7 @@ const BtnCont = styled.div`
   width: 100%;
 `;
 
-// const Icons = styled.div`
-//   display: flex;
-//   justify-content: flex-end;
-//   padding: 0.5rem;
-// `;
-export const Inputtextarea = styled.textarea`
+const Inputtextarea = styled.textarea`
   display: flex;
   line-height: 1.44em;
   /* border: 0; */
@@ -164,13 +161,13 @@ const TopicCard = (props) => {
 
   const inputfocus = useRef(null);
 
-  useEffect(() => {
-    if (editing || open) {
-      setTimeout(() => {
-        inputfocus.current.focus();
-      }, 0);
-    }
-  }, [editing, open]);
+  // useEffect(() => {
+  //   if (editing || open) {
+  //     setTimeout(() => {
+  //       inputfocus.current.focus();
+  //     }, 0);
+  //   }
+  // }, [editing, open]);
 
   const handledialogtoggle = () => {
     setDialogToggle(!dialogtoggle);
@@ -266,161 +263,161 @@ const TopicCard = (props) => {
         timeformat(postdates)
       : null;
 
-  const modalBody = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 className={classes.mHeader}>Twitter Manager</h2>
-      <h3 className={classes.mAccent}>Schedule or Post Now</h3>
-      <FormControl className={classes.formControl}>
-        <span className='socialAccountInput'>
-          <InputLabel
-            shrink
-            // className={classes.select}
-            className='test'
-            id='twitter-handle-select'
-          >
-            <span className='socialAccount'> Social Account </span>
-          </InputLabel>
-          <Select
-            labelId='twitter-handle-select'
-            id='select'
-            value={handle}
-            // defaultValue={1}
-            onChange={handleHandleChange}
-            className='test'
-            style={{ width: '40%' }}
-          >
-            <MenuItem defaultValue value={1}>
-              <span id='alignTextIcon'>
-                @{screen_name} &nbsp;{' '}
-                <TwitterIcon className='twitterIconModal' />
-              </span>
-            </MenuItem>
-          </Select>
-        </span>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <nav className='item-sub-nav'>
-            <NavLink
-              onClick={() => {
-                setPostNow(false);
-                handleDateChange(new Date());
-              }}
-              to={`/home/schedule`}
-            >
-              Schedule
-            </NavLink>
-            <NavLink
-              onClick={() =>
-                setPostNow(true) & setcontent({ ...content, date: '' })
-              }
-              to={`/home/post-now`}
-            >
-              Post Now
-            </NavLink>
-          </nav>
-          <Grid container justify='space-around' alignItems='center'>
-            {!postnow ? (
-              <>
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    margin: '5%',
-                  }}
-                >
-                  <DatePicker
-                    disableToolbar
-                    variant='inline'
-                    format='MM/dd/yyyy'
-                    margin='normal'
-                    id='date-picker-inline'
-                    disablePast={true}
-                    label='Date'
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                  />
-                  <TimePicker
-                    margin='normal'
-                    id='time-picker'
-                    label='Time'
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                  />
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <button
-                    onClick={() =>
-                      new Date(rectime) > new Date()
-                        ? handleDateChange(rectime)
-                        : newRecTime()
-                    }
-                    style={{
-                      borderRadius: '6px',
-                      width: '205px',
-                      height: '38px',
-                      background: '#817BAB',
-                      color: '#EBECF0',
-                    }}
-                  >
-                    Suggest a Time
-                  </button>
-                  <Tooltip
-                    title='proprietary optimization algorithm to maximize engagement'
-                    placement='top-end'
-                    aria-label='InfoIcon'
-                    fontSize='small'
-                    color='action'
-                  >
-                    <InfoIcon />
-                  </Tooltip>
-                </div>
-              </>
-            ) : null}
-          </Grid>
-        </MuiPickersUtilsProvider>
-        <Inputtextarea
-          placeholder='Customize your Twitter message here'
-          type='text'
-          name='post_text'
-          cols='50'
-          rows='10'
-          maxLength='280'
-          ref={inputfocus}
-          onFocus={handletweetchange}
-          value={content.post_text}
-          onChange={(event) => handletweetchange(event, handleChange(event))}
-        />
-        <span
-          style={
-            tweetCount.chars_left < 80
-              ? { color: 'red' }
-              : tweetCount.chars_left < 180
-              ? { color: 'orange' }
-              : null
-          }
-          className='tweetcountmodal'
-        >
-          {tweetCount.chars_left}
-        </span>
-      </FormControl>
-      {!postnow ? (
-        <button
-          onClick={onsubmitTwitter}
-          style={{ width: '40%' }}
-          className={classes.actionSubmit}
-        >
-          Schedule
-        </button>
-      ) : (
-        <button
-          onClick={onsubmitTwitter}
-          style={{ width: '40%' }}
-          className={classes.actionSubmit}
-        >
-          Post now
-        </button>
-      )}
-    </div>
-  );
+  // const modalBody = (
+  //   <div style={modalStyle} className={classes.paper}>
+  //     <h2 className={classes.mHeader}>Twitter Manager</h2>
+  //     <h3 className={classes.mAccent}>Schedule or Post Now</h3>
+  //     <FormControl className={classes.formControl}>
+  //       <span className='socialAccountInput'>
+  //         <InputLabel
+  //           shrink
+  //           // className={classes.select}
+  //           className='test'
+  //           id='twitter-handle-select'
+  //         >
+  //           <span className='socialAccount'> Social Account </span>
+  //         </InputLabel>
+  //         <Select
+  //           labelId='twitter-handle-select'
+  //           id='select'
+  //           value={handle}
+  //           // defaultValue={1}
+  //           onChange={handleHandleChange}
+  //           className='test'
+  //           style={{ width: '40%' }}
+  //         >
+  //           <MenuItem defaultValue value={1}>
+  //             <span id='alignTextIcon'>
+  //               @{screen_name} &nbsp;{' '}
+  //               <TwitterIcon className='twitterIconModal' />
+  //             </span>
+  //           </MenuItem>
+  //         </Select>
+  //       </span>
+  //       <MuiPickersUtilsProvider utils={DateFnsUtils}>
+  //         <nav className='item-sub-nav'>
+  //           <NavLink
+  //             onClick={() => {
+  //               setPostNow(false);
+  //               handleDateChange(new Date());
+  //             }}
+  //             to={`/schedule`}
+  //           >
+  //             Schedule
+  //           </NavLink>
+  //           <NavLink
+  //             onClick={() =>
+  //               setPostNow(true) & setcontent({ ...content, date: '' })
+  //             }
+  //             to={`/post-now`}
+  //           >
+  //             Post Now
+  //           </NavLink>
+  //         </nav>
+  //         <Grid container justify='space-around' alignItems='center'>
+  //           {!postnow ? (
+  //             <>
+  //               <div
+  //                 style={{
+  //                   display: 'flex',
+  //                   flexDirection: 'column',
+  //                   margin: '5%',
+  //                 }}
+  //               >
+  //                 <DatePicker
+  //                   disableToolbar
+  //                   variant='inline'
+  //                   format='MM/dd/yyyy'
+  //                   margin='normal'
+  //                   id='date-picker-inline'
+  //                   disablePast={true}
+  //                   label='Date'
+  //                   value={selectedDate}
+  //                   onChange={handleDateChange}
+  //                 />
+  //                 <TimePicker
+  //                   margin='normal'
+  //                   id='time-picker'
+  //                   label='Time'
+  //                   value={selectedDate}
+  //                   onChange={handleDateChange}
+  //                 />
+  //               </div>
+  //               <div style={{ display: 'flex', alignItems: 'center' }}>
+  //                 <button
+  //                   onClick={() =>
+  //                     new Date(rectime) > new Date()
+  //                       ? handleDateChange(rectime)
+  //                       : newRecTime()
+  //                   }
+  //                   style={{
+  //                     borderRadius: '6px',
+  //                     width: '205px',
+  //                     height: '38px',
+  //                     background: '#817BAB',
+  //                     color: '#EBECF0',
+  //                   }}
+  //                 >
+  //                   Suggest a Time
+  //                 </button>
+  //                 <Tooltip
+  //                   title='proprietary optimization algorithm to maximize engagement'
+  //                   placement='top-end'
+  //                   aria-label='InfoIcon'
+  //                   fontSize='small'
+  //                   color='action'
+  //                 >
+  //                   <InfoIcon />
+  //                 </Tooltip>
+  //               </div>
+  //             </>
+  //           ) : null}
+  //         </Grid>
+  //       </MuiPickersUtilsProvider>
+  //       <Inputtextarea
+  //         placeholder='Customize your Twitter message here'
+  //         type='text'
+  //         name='post_text'
+  //         cols='50'
+  //         rows='10'
+  //         maxLength='280'
+  //         ref={inputfocus}
+  //         onFocus={handletweetchange}
+  //         value={content.post_text}
+  //         onChange={(event) => handletweetchange(event, handleChange(event))}
+  //       />
+  //       <span
+  //         style={
+  //           tweetCount.chars_left < 80
+  //             ? { color: 'red' }
+  //             : tweetCount.chars_left < 180
+  //             ? { color: 'orange' }
+  //             : null
+  //         }
+  //         className='tweetcountmodal'
+  //       >
+  //         {tweetCount.chars_left}
+  //       </span>
+  //     </FormControl>
+  //     {!postnow ? (
+  //       <button
+  //         onClick={onsubmitTwitter}
+  //         style={{ width: '40%' }}
+  //         className={classes.actionSubmit}
+  //       >
+  //         Schedule
+  //       </button>
+  //     ) : (
+  //       <button
+  //         onClick={onsubmitTwitter}
+  //         style={{ width: '40%' }}
+  //         className={classes.actionSubmit}
+  //       >
+  //         Post now
+  //       </button>
+  //     )}
+  //   </div>
+  //);
 
   return (
     <Draggable draggableId={String(props.id)} index={props.index}>
@@ -458,10 +455,6 @@ const TopicCard = (props) => {
                       </span>
                     </>
                   )}
-
-                  {/* <span style={{ fontSize: '10px' }}>
-                    <TwitterIcon fontSize="inherit" />
-                  </span> */}
                 </a>
 
                 <span style={{ color: '#848484', fontSize: '9px' }}>
@@ -483,17 +476,6 @@ const TopicCard = (props) => {
             </div>
 
             <span className='editIcons'>
-              {/* <IconButton
-                disableRipple={true}
-                disableFocusRipple={true}
-                className={classes.button}
-                style={{
-                  display: 'flex',
-                  alignItems: 'flex-start',
-                  paddingTop: '0',
-                  paddingRight: '0',
-                }}
-              > */}
               {postContent?.date?.length &&
               new Date(postContent?.date) < new Date() ? null : postContent
                   ?.date?.length ? null : (
@@ -510,13 +492,11 @@ const TopicCard = (props) => {
                   >
                     <CreateIcon
                       className={`${props.card.id}-create`}
-                      // onClick={() => setediting(!editing)}
                       fontSize='small'
                     />
                   </IconButton>
                 </span>
               )}
-              {/* </IconButton> */}
               <IconButton
                 disableRipple={true}
                 disableFocusRipple={true}
@@ -530,7 +510,6 @@ const TopicCard = (props) => {
                 <MoreVertIcon
                   className={`${props.card.id}-edit`}
                   style={{ padding: '0rem .25rem' }}
-                  // onClick={handleClick}
                   fontSize='small'
                 />
               </IconButton>
@@ -577,7 +556,6 @@ const TopicCard = (props) => {
 
           <Dialog
             open={dialogtoggle}
-            // onClose={handledialogtoggle}
             disableEscapeKeyDown={true}
             aria-labelledby='alert-dialog-title'
             aria-describedby='alert-dialog-description'
@@ -621,7 +599,7 @@ const TopicCard = (props) => {
               timeout: 500,
             }}
           >
-            <Fade in={open}>{modalBody}</Fade>
+            <Fade in={open}><CardModal card={props.card} setOpen={setOpen}/></Fade>
           </Modal>
           {!editing ? (
             <Typography
@@ -651,14 +629,11 @@ const TopicCard = (props) => {
                   maxLength='280'
                   ref={inputfocus}
                   onFocus={handletweetchange}
-                  // onClick={handletweetchange}
                   value={content.post_text}
                   onChange={(event) =>
                     handletweetchange(event, handleChange(event))
                   }
                 />
-                {/* &nbsp;{" "} */}
-
                 <div className='action-cont editcard-cont'>
                   <input className='actionSubmit' type='submit' />
                   <span
