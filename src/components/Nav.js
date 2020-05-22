513cb4e85484ca977210c78297634cfda420e6e5
fbzr <fabricio.spb@gmail.com>
2020-05-22 12:20:44 -0700

    refactor Nav component: create Search component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { makeStyles, fade } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import { Search, AccountCircle } from '@material-ui/icons';
-import InputBase from '@material-ui/core/InputBase';
-
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import { useOktaAuth } from '@okta/okta-react/dist/OktaContext';
+// Material-UI
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  ListItemIcon,
+  ListItemText,
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  InputBase,
+  MenuItem,
+  Menu
+} from '@material-ui/core';
+// Material-UI icons
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import { AccountCircle } from '@material-ui/icons';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+//Components
+import Search from './Search';
+
 import logo from '../assets/imgs/logo.png';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,121 +38,82 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
-  },
-  logo: {
-    fontFamily: 'Sansita'
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',   
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
+  }
 }));
 
 const Nav = ({ toggleMenu }) => {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
+  const { authService } = useOktaAuth();
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+      setAnchorEl(null);
+  };
 
+  const logout = async () => {
+    authService.logout('/');
+  }
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                <IconButton onClick={() => toggleMenu()} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
+  return (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+            <IconButton onClick={() => toggleMenu()} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            <Button>
+              <img src={logo} alt="SoMe logo"/>
+            </Button>
+            <Search />
+            <div style={{flexGrow: '1', textAlign: 'end'}}>
+                <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+                >
+                    <AccountCircle />
                 </IconButton>
-                <Button>
-                  <img src={logo} alt="SoMe logo"/>
-                </Button>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <Search />
-                    </div>
-                    <InputBase 
-                        placeholder="Search…"
-                        classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'search' }}
-                    />
-                </div>
-                <div style={{flexGrow: '1', textAlign: 'end'}}>
-                    <IconButton
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                    >
-                        <AccountCircle />
-                    </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorEl}
-                        anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                        }}
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
-                </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    getContentAnchorEl={null}
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                      <ListItemIcon>
+                        <AccountBoxIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText primary="Profile" />
+                    </MenuItem>
+                    <MenuItem onClick={logout}>
+                      <ListItemIcon>
+                        <ExitToAppIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText primary="Logout" />
+                    </MenuItem>
+                </Menu>
+            </div>
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
 }
 
 export default Nav;
