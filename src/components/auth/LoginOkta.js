206e5fa97d56247c4b1a93df255e2cb5432c9b28
fbzr <fabricio.spb@gmail.com>
2020-05-24 14:42:02 -0700

    design okta login/signup widget

--- src/components/auth/LoginOkta.js
+++ src/components/auth/LoginOkta.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import SignInWidget from './SignInWidget';
-import { Redirect } from 'react-router-dom';
-import { Grid, Hidden, makeStyles } from '@material-ui/core';
+import { Redirect, useHistory } from 'react-router-dom';
+import { Grid, Hidden, makeStyles, AppBar, Toolbar, Button, CircularProgress } from '@material-ui/core';
 import loginImg from '../../assets/imgs/login.png';
+import logo from '../../assets/imgs/Logo.svg';
+import logoDark from '../../assets/imgs/Logo-dark.svg';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100vh'
+    },
+    loading: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    },
+    navbar: {
+        height: theme.navbar.height.small,
+        display: 'flex',
+        justifyContent: 'center'
+    },
     imageContainer: {
         overflow: 'hidden',
-        maxHeight: '100vh'
+        height: '100%'
     },
     image: {
         width: '100%',
         height: '100%',
         objectFit: 'cover',
         objectPosition: 'center'
+    },
+    logo: {
+        height: '20px'
+    },
+    widgetContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
     }
-});
+}));
 
 const LoginOkta = ({ baseUrl }) => {
     const { authService, authState } = useOktaAuth();
+    const { push } = useHistory();
     const classes = useStyles();
 
     const onSuccess = async res => {
@@ -29,15 +55,39 @@ const LoginOkta = ({ baseUrl }) => {
         });
     };
 
+    const handleLogoClick = () => {
+        push('/');
+    }
+
     if(authState.isPending) { 
-        return <div>Loading...</div>;
+        return (
+            <div className={classes.loading}>
+                <CircularProgress />
+            </div>
+        );
     }
     
     return ( authState.isAuthenticated ?
         <Redirect to='/app' />
         :
-        <Grid container wrap='nowrap'>
-            <Grid item xs>
+        <Grid container wrap='wrap' className={classes.root}>
+            <Hidden mdUp>
+                <Grid xs={12}>
+                    <AppBar className={classes.navbar} position='static'>
+                        <Toolbar>
+                            <Button onClick={handleLogoClick}>
+                                <img className={classes.logo} src={logo} alt="SoMe logo"/>
+                            </Button>
+                        </Toolbar>
+                    </AppBar>
+                </Grid>
+            </Hidden>
+            <Grid item xs className={classes.widgetContainer}>
+                <Hidden mdDown>
+                    <Button onClick={handleLogoClick} style={{position: 'absolute', top:'.8em', left:'.8em'}}>
+                        <img className={classes.logo} src={logoDark} alt="SoMe logo"/>
+                    </Button>
+                </Hidden>
                 <SignInWidget baseUrl={baseUrl} onSuccess={onSuccess} />
             </Grid>
             <Hidden xsDown>
--- src/components/auth/SignInWidget.js
+++ src/components/auth/SignInWidget.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from 'react';
 import OktaSignIn from '@okta/okta-signin-widget';
+import { makeStyles } from '@material-ui/core';
 // import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
-import './SignInWidget.css'
+// import './SignInWidget.css';
+import './SignInWidget.scss';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+}));
 
 const OktaSigninWidget = ({baseUrl, onSuccess}) => {
+    const classes = useStyles();
+
     useEffect(() => {
         const widget = new OktaSignIn({
-            baseUrl
+            baseUrl,
+            features: {
+                registration: true
+            }
         });
 
         widget.renderEl({ el: '#sign-in-widget' }, onSuccess);
@@ -14,7 +29,7 @@ const OktaSigninWidget = ({baseUrl, onSuccess}) => {
     
 
     return (
-        <div className='widget-container'>
+        <div className={classes.root}>
             <div id='sign-in-widget' />
         </div>
     );
