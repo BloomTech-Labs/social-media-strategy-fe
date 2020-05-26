0c2527c9d8c99a78edd53b0700d2ea2d6802699c
Tim Shaker <tim@tmshkr.com>
2020-05-26 08:45:56 -0700

    update axiosWithAuth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-require("dotenv").config();
 
 const url = process.env.REACT_APP_API_URL || "https://api.so-me.net/api/";
 
@@ -8,7 +7,7 @@ export const axiosWithAuth = () => {
   return axios.create({
     baseURL: url,
     headers: {
-      Authorization: `Bearer ${oktaToken.accessToken.accessToken}`,
+      Authorization: `Bearer ${oktaToken.accessToken.value}`,
     },
   });
 };
