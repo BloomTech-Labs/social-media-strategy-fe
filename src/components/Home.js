08a5926b9fa370f520aaa6750968c936260c2bff
jackskim <jackskim29@gmail.com>
2020-05-26 17:23:16 -0700

    Add user.twitter_screenName check and history.push()

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
 // Pages
 import MediaManager from "./dashboard/MediaManager";
-import ConnectAccounts from "./auth/ConnectAccounts";
 // Components
 import Nav from "./Nav";
 import DrawerMenu from "./DrawerMenu";
@@ -11,17 +10,21 @@ import DrawerMenu from "./DrawerMenu";
 const Home = () => {
   const { authService } = useOktaAuth();
   const [menuOpen, setMenuOpen] = useState(false);
-  
+
   // TODO: update user info in redux store
   // TODO: Using component state now. Change to react-redux useSelector
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({});
+  const history = useHistory();
 
   useEffect(() => {
-    if(!user) {
+    if (!user.email) {
       // load user info
       (async () => {
         const oktaUser = await authService.getUser();
         setUser(oktaUser);
+        if (!user.twitter_screenName) {
+          history.push("/connect/twitter");
+        }
       })();
     }
   }, [authService, user]);
@@ -30,20 +33,19 @@ const Home = () => {
     setMenuOpen(!menuOpen);
   };
 
-  return (user && 
-    <>
-      <Nav toggleMenu={toggleMenu} />
-      <DrawerMenu open={menuOpen} toggleMenu={toggleMenu} />
+  return (
+    user && (
+      <>
+        <Nav toggleMenu={toggleMenu} />
+        <DrawerMenu open={menuOpen} toggleMenu={toggleMenu} />
 
-      <main>
-        <Route exact path={['/app', '/app/media-manager']}>
-          <MediaManager user={user} />
-        </Route>
-        <Route exact path='/app/connect'>
-          <ConnectAccounts user={user} />
-        </Route>
-      </main>
-    </>
+        <main>
+          <Route exact path={["/app", "/app/media-manager"]}>
+            <MediaManager user={user} />
+          </Route>
+        </main>
+      </>
+    )
   );
 };
 
