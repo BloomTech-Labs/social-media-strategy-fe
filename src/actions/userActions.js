e85140753f152b1741ac71cb10e3ec5c0adb67d1
Tim Shaker <tim@tmshkr.com>
2020-05-31 14:25:23 -0700

    INITIALIZE_USER actions and types

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,1 +1,20 @@
-// SET_USER from Okta token
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+import { INITIALIZE_USER, AUTHORIZE_TWITTER } from "./userActionTypes";
+
+export const initializeUser = (authService) => async (dispatch) => {
+  axiosWithAuth(authService).put(`users`);
+  const { sub, email, twitter_handle } = await authService.getUser();
+
+  dispatch({
+    type: INITIALIZE_USER,
+    payload: {
+      initialized: true,
+      okta_uid: sub,
+      email,
+      twitter_handle,
+    },
+  });
+};
+
+export const authorizeTwitter = () => (dispatch) => {};
