a1cff506e77bae34f6abe33ab916de6ee4fcf95b
Tim Shaker <tim@tmshkr.com>
2020-05-31 14:24:35 -0700

    use Okta authService with axiosWithAuth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_API_URL || "https://api.so-me.net/api/";
 
-export const axiosWithAuth = () => {
-  const oktaToken = JSON.parse(localStorage.getItem("okta-token-storage"));
+export const axiosWithAuth = (authService) => {
+  const { accessToken } = authService.getAuthState();
+
   return axios.create({
     baseURL: url,
     headers: {
-      Authorization: `Bearer ${oktaToken.accessToken.value}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
 };
