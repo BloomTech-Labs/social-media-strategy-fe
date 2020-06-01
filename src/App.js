269edf8b2c2197d9bd15916fb33504fb75d55173
Tim Shaker <tim@tmshkr.com>
2020-05-31 17:10:44 -0700

    move Security to index.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { SecureRoute, LoginCallback } from "@okta/okta-react";
+import { Route, Switch } from "react-router";
 
-import "./sass/index.scss";
-import { Route, Switch, useHistory } from "react-router";
-// import Login from "./components/auth/Login";
-// import PrivateRoute from "./utils/PrivateRoute";
-// import Team from "./components/Team";
-// import HomePage from "./components/HomePage";
-// import Callback from "./components/Callback";
-// import Registration from "./components/auth/Registration";
 import Landing from "./components/Landing";
-// new stuff
-import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
 import LoginOkta from "./components/auth/LoginOkta";
 import ConnectAccounts from "./components/auth/ConnectAccounts";
 import Home from "./components/Home";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
-const config = {
-  issuer: `${process.env.REACT_APP_OKTA_DOMAIN}/oauth2/default`,
-  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
-  redirectUri: window.location.origin + "/implicit/callback",
-  scopes: ["openid", "profile", "email"],
-};
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./sass/index.scss";
 
 const App = (props) => {
-  const { push } = useHistory();
-
   return (
-    <Security {...config} onAuthRequired={() => push("/login")}>
+    <Fragment>
       <CssBaseline />
       <Switch>
         <Route exact path="/" component={Landing} />
@@ -38,7 +23,7 @@ const App = (props) => {
         <SecureRoute path="/connect" component={ConnectAccounts} />
         <Route path="/implicit/callback" component={LoginCallback} />
       </Switch>
-    </Security>
+    </Fragment>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./muiCustomTheme";
-
-// imports for redux setup
+import { Security } from "@okta/okta-react";
 import { createStore, applyMiddleware, compose } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+
+import App from "./App";
+
+// imports for redux setup
 import { reducer } from "./reducers";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./muiCustomTheme";
+import "./index.css";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -20,29 +22,25 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, logger))
 );
 
-//  TO MAKE LOGGER ONLY SHOW UP IN DEV----------------------
-// const middlewares = [];
-
-// if (process.env.NODE_ENV === `development`) {
-//   const { logger } = require(`redux-logger`);
-
-//   middlewares.push(logger);
-// }
-
-// const store = compose(applyMiddleware(...middlewares))(createStore)(reducer);
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_DOMAIN}/oauth2/default`,
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  redirectUri: window.location.origin + "/implicit/callback",
+  scopes: ["openid", "profile", "email"],
+};
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </Router>
-  </Provider>,
+  <Security
+    {...oktaConfig}
+    onAuthRequired={() => (window.location.href = "/login")}
+  >
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
+  </Security>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
