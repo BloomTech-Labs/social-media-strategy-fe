95e0aa7c948ba495aa5b2c63f6968e22b7147a18
fbzr <fabricio.spb@gmail.com>
2020-06-14 14:31:03 -0700

    add modal alert to delete post

--- src/components/kanban/PostMenu.js
+++ src/components/kanban/PostMenu.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deletePost } from "../../actions/listsActions";
+import ModalAlert from "../templates/ModalAlert";
 // Material-UI
 import {
-  IconButton,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-  Menu,
+	IconButton,
+	ListItemIcon,
+	ListItemText,
+	MenuItem,
+	Menu,
 } from "@material-ui/core";
 // Material-UI icons
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
@@ -15,72 +16,84 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 
 const PostMenu = ({ post, setEditing }) => {
-  const dispatch = useDispatch();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const isOpen = Boolean(anchorEl);
+	const dispatch = useDispatch();
+	const [modalOpen, setModalOpen] = useState(false);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const isOpen = Boolean(anchorEl);
 
-  const handleAnchorEl = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
+	const handleAnchorEl = (e) => {
+		setAnchorEl(e.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleEdit = () => {
-    setEditing();
-  };
+	const handleEdit = () => {
+		setEditing();
+	};
 
-  const handleDelete = async () => {
-    dispatch(deletePost(post));
-    handleClose();
-  };
+	const handleDeleteClick = () => {
+		setModalOpen(true);
+		handleClose();
+	};
 
-  return (
-    <>
-      <IconButton
-        aria-label="account of current user"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
-        onClick={handleAnchorEl}
-        color="inherit"
-      >
-        <MoreHorizIcon fontSize="small" />
-      </IconButton>
+	const handleDeleteConfirmation = async () => {
+		dispatch(deletePost(post));
+		handleClose();
+	};
 
-      <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        getContentAnchorEl={null}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        open={isOpen}
-        onClose={handleClose}
-      >
-        { !post.posted && 
-          <MenuItem onClick={handleEdit}>
-            <ListItemIcon>
-                <EditIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText primary="Edit" />
-          </MenuItem> 
-        }
-        <MenuItem onClick={handleDelete}>
-          <ListItemIcon>
-            <DeleteIcon fontSize="small" />
-          </ListItemIcon>
-          <ListItemText primary="Delete" />
-        </MenuItem>
-      </Menu>
-    </>
-  );
+	return (
+		<>
+			<ModalAlert
+				open={modalOpen}
+				setOpen={setModalOpen}
+				title="Delete this post?"
+				handleConfirmation={handleDeleteConfirmation}
+			/>
+			<IconButton
+				aria-label="account of current user"
+				aria-controls="menu-appbar"
+				aria-haspopup="true"
+				onClick={handleAnchorEl}
+				color="inherit"
+			>
+				<MoreHorizIcon fontSize="small" />
+			</IconButton>
+
+			<Menu
+				id="menu-appbar"
+				anchorEl={anchorEl}
+				getContentAnchorEl={null}
+				anchorOrigin={{
+					vertical: "bottom",
+					horizontal: "right",
+				}}
+				keepMounted
+				transformOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				open={isOpen}
+				onClose={handleClose}
+			>
+				{!post.posted && (
+					<MenuItem onClick={handleEdit}>
+						<ListItemIcon>
+							<EditIcon fontSize="small" />
+						</ListItemIcon>
+						<ListItemText primary="Edit" />
+					</MenuItem>
+				)}
+				<MenuItem onClick={handleDeleteClick}>
+					<ListItemIcon>
+						<DeleteIcon fontSize="small" />
+					</ListItemIcon>
+					<ListItemText primary="Delete" />
+				</MenuItem>
+			</Menu>
+		</>
+	);
 };
 
 export default PostMenu;
