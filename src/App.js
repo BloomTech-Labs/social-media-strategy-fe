c9ec96b752d9d9caf77ad8055e3d55add92825a6
jackskim <jackskim29@gmail.com>
2020-06-14 15:56:31 -0700

    Remove unused props and console.logs

--- src/App.js
+++ src/App.js
@@ -13,16 +13,14 @@ import Home from "./components/pages/Home";
 import Analytics from "./components/pages/Analytics";
 import NavMenuTemplate from "./components/templates/NavMenuTemplate";
 
-function App(props) {
+function App() {
   const { authService } = useOktaAuth();
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
-  const user = useSelector((state) => state.user);
+  const user = useSelector(state => state.user);
 
   useEffect(() => {
-    console.log("App.js useEffect fired", Date.now());
-    console.log(location);
     if (location.pathname === "/connect/twitter/callback") return;
     if (!user.initialized) {
       dispatch(initializeUser(authService, history));
