e9e79016b188f24040d8c3427ff10df1e71dbf47
fbzr <fabricio.spb@gmail.com>
2020-06-14 17:12:47 -0700

    increase height of kanban scrollbar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,44 +6,59 @@ import Kanban from "../kanban/Kanban";
 import Scrollbars from "react-custom-scrollbars";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.secondary,
-    position: "relative",
-    overflow: "hidden",
-    height: "100vh",
-    width: "100%",
-  },
-  topContainer: {
-    height: theme.kanban.topContainer.height,
-    display: "flex",
-  },
-  header: {
-    margin: theme.spacing(2),
-    width: "100%",
-    textAlign: "left",
-  },
-  scrollbarContainer: {
-    height: `calc(100vh - ${theme.kanban.topContainer.height})`,
-  },
+	root: {
+		backgroundColor: theme.palette.background.secondary,
+		position: "relative",
+		overflow: "hidden",
+		height: "100vh",
+		width: "100%",
+	},
+	topContainer: {
+		height: theme.kanban.topContainer.height,
+		display: "flex",
+	},
+	header: {
+		margin: theme.spacing(2),
+		width: "100%",
+		textAlign: "left",
+	},
+	scrollbarContainer: {
+		height: `calc(100vh - ${theme.kanban.topContainer.height})`,
+	},
 }));
 
 const Home = () => {
-  const { root, header, scrollbarContainer, topContainer } = useStyles();
+	const { root, header, scrollbarContainer, topContainer } = useStyles();
 
-  return (
-    <div className={root}>
-      <div className={topContainer}>
-        <Typography variant="h4" className={header}>
-          Media Manager
-        </Typography>
-      </div>
-      <div className={scrollbarContainer}>
-        <Scrollbars>
-          <Kanban />
-        </Scrollbars>
-      </div>
-    </div>
-  );
+	return (
+		<div className={root}>
+			<div className={topContainer}>
+				<Typography variant="h4" className={header}>
+					Media Manager
+				</Typography>
+			</div>
+			<div className={scrollbarContainer}>
+				<Scrollbars
+					renderTrackHorizontal={({ style, ...props }) => (
+						<div
+							{...props}
+							style={{
+								...style,
+								width: "100%",
+								height: "8px",
+								position: "absolute",
+								bottom: "0",
+								borderRadius: "15px",
+								marginBottom: "5px",
+							}}
+						/>
+					)}
+				>
+					<Kanban />
+				</Scrollbars>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
