d2f14f22699375ffc8c5545a1b6d6f12bb7d385b
Aja Blanco <aja@Ajas-MBP.attlocal.net>
2020-06-14 17:45:03 -0700

    centered analytics title

--- src/components/pages/Analytics.js
+++ src/components/pages/Analytics.js
@@ -2,23 +2,22 @@ import React, { Fragment } from "react";
 import Stats from "../analytics/Stats";
 import PopularWords from "../analytics/PopularWords";
 
-
-
 const Analytics = () => {
   return (
     <Fragment>
-      <h1
-        style={{
-          fontFamily: "Roboto Condensed",
-          color: "#4E4E4E",
-          marginLeft: "15%",
-          fontSize: "30pt",
-        }}
-      >
-        Analytics
-      </h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1
+          style={{
+            fontFamily: "Roboto Condensed",
+            color: "#4E4E4E",
+            fontSize: "30pt",
+          }}
+        >
+          Analytics
+        </h1>
+      </div>
       <Stats />
-      <PopularWords/>
+      <PopularWords />
     </Fragment>
   );
 };
