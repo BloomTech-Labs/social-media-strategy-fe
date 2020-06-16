f0dae4eaf624909386d6e3393bf01a66a13bc274
fbzr <fabricio.spb@gmail.com>
2020-06-16 09:20:22 -0700

    get status working

--- src/actions/popwordsActions.js
+++ src/actions/popwordsActions.js
@@ -1,29 +1,57 @@
 import axios from "axios";
-
-export const SEARCH_REQUEST = 'SEARCH_REQUEST';
-export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
-export const SEARCH_FAILURE = 'SEARCH_FAILURE';
+import { UPDATE_STATUS, SET_TOPICS, ERROR } from "./popwordsActionTypes";
 
 export function getWords() {
-    return async (dispatch, getState) => {
-    const body = { twitter_handle: `${getState().user.twitter_handle}`};
-    
-    dispatch({
-        type: SEARCH_REQUEST
-    });
+	return async (dispatch, getState) => {
+		const body = { twitter_handle: `${getState().user.twitter_handle}` };
 
-    axios
-    .post("http://so-me-fastapi.eba-ghirpj73.us-east-1.elasticbeanstalk.com/topic_model/get_topics", body)
-    .then(res => {
-        console.log(res);
-        console.log('response!!!', res.data);
-        dispatch({ type: SEARCH_SUCCESS, payload: res.data });
-    })
-    .catch(err => {
-        dispatch({
-            type: SEARCH_FAILURE,
-            payload: err,    
-        });     
-    });
-};
+		axios
+			.post(
+				"http://so-me-fastapi.eba-ghirpj73.us-east-1.elasticbeanstalk.com/topic_model/get_topics",
+				body,
+			)
+			.then((res) => {
+				const topics = Object.values(res.data.topics);
+				dispatch({ type: SET_TOPICS, payload: topics });
+			})
+			.catch((err) => {
+				dispatch({
+					type: ERROR,
+					payload: err,
+				});
+			});
+	};
 }
+
+export const getStatus = (twitter_handle) => async (dispatch, getState) => {
+	const body = {
+		twitter_handle,
+	};
+	const { data } = await axios.post(
+		"http://so-me-fastapi.eba-ghirpj73.us-east-1.elasticbeanstalk.com/topic_model/status",
+		body,
+	);
+
+	dispatch({ type: UPDATE_STATUS, payload: data });
+};
+
+export const requestPopWords = (
+	numFollowers = 500,
+	maxAge = 7,
+	ignoredWords = [],
+) => async (dispatch, getState) => {
+	const body = {
+		twitter_handle: `${getState().user.twitter_handle}`,
+		num_followers_to_scan: numFollowers,
+		max_age_of_tweet: maxAge,
+		words_to_ignore: ignoredWords,
+	};
+
+	await axios.post(
+		"http://so-me-fastapi.eba-ghirpj73.us-east-1.elasticbeanstalk.com/topic_model/schedule",
+		body,
+	);
+
+	// update status
+	await dispatch(getStatus());
+};
--- src/components/pages/Analytics.js
+++ src/components/pages/Analytics.js
@@ -3,23 +3,23 @@ import Stats from "../analytics/Stats";
 import PopularWords from "../analytics/PopularWords";
 
 const Analytics = () => {
-  return (
-    <Fragment>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1
-          style={{
-            fontFamily: "Roboto Condensed",
-            color: "#4E4E4E",
-            fontSize: "30pt",
-          }}
-        >
-          Analytics
-        </h1>
-      </div>
-      <Stats />
-      <PopularWords />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<h1
+					style={{
+						fontFamily: "Roboto Condensed",
+						color: "#4E4E4E",
+						fontSize: "30pt",
+					}}
+				>
+					Analytics
+				</h1>
+			</div>
+			<Stats />
+			<PopularWords />
+		</Fragment>
+	);
 };
 
 export default React.memo(Analytics);
